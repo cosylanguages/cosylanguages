4f96797734f78b67402be69939693cd0c2a6304b
cosylanguages <cosylanguages@gmail.com>
2025-05-23 20:45:11 +0200

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Select DOM elements
+    const languageSelect = document.getElementById("language-select");
+    const daySelect = document.getElementById("day-select");
+    const dayFromSelect = document.getElementById("day-from-select");
+    const dayToSelect = document.getElementById("day-to-select");
+    const practiceTypeSelect = document.getElementById("practice-type-select");
+    const showButton = document.getElementById("show-button");
+    const resultContainer = document.getElementById("result-container");
+
+showButton.addEventListener("click", () => {
+  // Get user selections
+  // Call randomiser functions depending on practiceType
+});
