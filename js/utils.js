c9663c7b60a5ee6f053c26bd51f45acd7bb41bbb
cosylanguages <cosylanguages@gmail.com>
2025-05-23 22:56:07 +0200

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,10 +1,7 @@
-// Function to get random element from array
-    function randomElement(arr) {
-      return arr[Math.floor(Math.random() * arr.length)];
-    }
+function randomElement(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
 
-// Validate range input: dayFrom <= dayTo
-    function validDayRange(from, to) {
-      if (!from || !to) return false;
-      return Number(from) <= Number(to);
-    }
+function validDayRange(from, to) {
+  return Number(from) <= Number(to);
+}
