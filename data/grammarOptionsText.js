6308039cd1ea43131b20a9e6e1efd09dc084a732
cosylanguages <cosylanguages@gmail.com>
2025-05-23 20:34:05 +0200

    Update grammarOptionsText.js

--- data/grammarOptionsText.js
+++ data/grammarOptionsText.js
@@ -1,1 +1,12 @@
-
+// Grammar options text in all languages
+    const grammarOptionsText = {
+      COSYenglish: { to_have: "To Have", possessives: "Possessive Adjectives" },
+      COSYfrançais: { to_have: "Avoir", possessives: "Adjectifs Possessifs" },
+      COSYitaliano: { to_have: "Avere", possessives: "Aggettivi Possessivi" },
+      COSYespañol: { to_have: "Tener", possessives: "Adjetivos Posesivos" },
+      COSYportuguês: { to_have: "Ter", possessives: "Adjetivos Possessivos" },
+      COSYdeutsch: { to_have: "Haben", possessives: "Possessivartikel" },
+      ΚΟΖΥελληνικά: { to_have: "Έχω", possessives: "Κτητικά Επίθετα" },
+      ТАКОЙрусский: { to_have: "Иметь", possessives: "Притяжательные Прилагательные" },
+      ԾՈՍՅհայկական: { to_have: "Ունենալ", possessives: "Ստացական Ածականներ" }
+    };
